9cca993c706d19174d709bc631d8e075de8dd962
mocamoca <noi_kaas@yahoo.co.jp>
2018-03-27 10:32:17 +0900

    fabric.py: update README.md: update 写経内容を記載

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 fabric
 
 fabfile.pyを試すサンプル
+expert Python第二版の6章fablicを写経
--- fabric.py
+++ fabric.py
@@ -23,4 +23,45 @@ PYPI_URL = 'http://devpi.webxample.example.com'
 
 REMOTE_PROJECT_LOCATION = "/var/projects//webxample"
 
-env.project_locations
+env.project_location = REMOTE_PROJECT_LOCATION
+
+# roledefs は環境別(staging/production)の設定を持ちます
+env.roledefs = {
+    'staging': [
+        'staging.webxample.example.com',
+    ],
+    'production': [
+        'prod1.webexample.example.com',
+        'prod2.webexample.example.com',
+    ],
+}
+
+
+def prepare_release():
+    """ 新しいリリースのために、ソースの配布物を作って専用の
+    パッケージインデックスにアップロードします。
+    """
+    local('python setup.py build sdist upload -r {}'.format(
+        PYPI_URL
+    ))
+    
+    
+def get_version():
+    """ setuptools経由で、現在のヴァージョンを取得します。 """
+    return local(
+        'python setup.py --version', capture=True
+    ).stdout.strip()
+
+
+def switch_versions(version):
+    """ シンボリックリンクを差し替えることでアトミックにヴァージョンをきりかえます """
+    new_version_path = os.path.join(REMOTE_PROJECT_LOCATION, version)
+    temporary = os.path.join(REMOTE_PROJECT_LOCATION,'next')
+    desired = os.path.join(REMOTE_PROJECT_LOCATION, 'current')
+
+    # すでにある場合も強制的に(-f)シンボリックリンクを作成します。
+    run(
+        " ln -fsT {target} {symlink}"
+        "".format(tårget=new_version_path, symlink=temporary)
+    )
+
